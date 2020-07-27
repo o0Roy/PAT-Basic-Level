8e38a635e7c2f647ab630b8282192f9058674243
o0Roy <hsq_roy@163.com>
2020-07-27 18:00:05 +0800

    重命名知识点

--- test.go
+++ test.go
@@ -1,25 +1,16 @@
 package main
 
-import "fmt"
-
+import (
+	"fmt"
+	"github.com/golang-collections/collections/stack"
+)
 /**
 * @Author: hsq_roy
 * @Date: 2020/7/23 11:31 下午
 * @description：
  */
 func main() {
-<<<<<<< HEAD
-
-	三元运算组
-=======
-	num := 579
-	for {
-		num = num / 8
-		fmt.Println(num)
-		if num == 0 {
-			break
-		}
-	}
-	stack
->>>>>>> 5271030313acce1377226b2389d9ec8d3e42cb87
+	stk := stack.New()
+	stk.Push(1)
+	fmt.Println(stk.Pop())
 }
